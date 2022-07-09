609b4af4a9ff52fe54e700bebb467393341e8f88
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-07-09 17:51:34 -0300

    get Github repos for profile

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 // @route  GET api/posts
 // @desc   Test route
 // @access Public
-router.get('/', (req,res) => res.send('Posts Route'));
+router.get('/', (req, res) => res.send('Posts Route'))
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const connectDB = require('./config/db');
+const express = require('express')
+const connectDB = require('./config/db')
 
-const app = express();
+const app = express()
 
 // Connect Database
-connectDB();
+connectDB()
 
 // Init Middleware
-app.use(express.json({ extended:false }));
+app.use(express.json({ extended: false }))
 
-app.get('/', (req,res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running'))
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-app.listen(PORT , () => console.log(`Server started on port ${PORT}`));
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
