a9cf597d55c22772c818fc9cc6be1dad0c145b72
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-07-11 12:58:25 -0300

    delete and post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,7 @@ const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
 
-// @route  GET api/posts
+// @route  POST api/posts
 // @desc   Create a post
 // @access Private
 router.post(
@@ -22,7 +22,7 @@ router.post(
         try {
             const user = await User.findById(req.user.id).select('-password')
 
-            const newPost = new Post ({
+            const newPost = new Post({
                 text: req.body.text,
                 name: user.name,
                 avatar: user.avatar,
@@ -38,5 +38,62 @@ router.post(
         }
     }
 )
+
+// @route  GET api/posts
+// @desc   GET all posts
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({date: -1});
+        res.json(posts)
+    } catch (error) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+})
+
+// @route  GET api/posts/:id
+// @desc   GET post by id
+// @access Private
+
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' })
+        }
+        res.json(post);
+    } catch (err) {
+        console.error(err.message)
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' })        
+        }
+        res.status(500).send('Server error')
+    }
+})
+
+// @route  DELETE api/posts/:id
+// @desc   Delete a post
+// @access Private
+
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' })
+        }
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' })
+        }
+        await post.remove()
+        res.json({ msg: 'Post removed' })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+})
+
 
 module.exports = router
