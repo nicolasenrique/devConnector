1d27edd761f14db3ca3abdb6ad4d48c23b38ec89
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-07-12 12:29:51 -0300

    register form and useState hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
-import './App.css';
-import React, { Fragment } from 'react';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
+import './App.css'
+import React, { Fragment } from 'react'
+import Navbar from './components/layout/Navbar'
+import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-const App = () => 
-    <Fragment>
-        <Navbar />
-        <Landing />
-    </Fragment>
+const App = () => (
+    <Router>
+        <Fragment>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </Fragment>
+    </Router>
+)
 
-export default App;
+export default App
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default login
+
